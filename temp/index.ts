e9e1d1781a500b5a7cb52b4ab5b208f7111201a4
t0ffee <spicyn.v2001@gmail.com>
2022-11-10 17:17:05 +0700

    chore: убрал мусор, обновил tsconfig.json

--- temp/index.ts
+++
@@ -1,3 +0,0 @@
-type Immutable<T> = { readonly [P in keyof T]: T[P] };
-// https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-4.html#const-assertions
-// as const - Крутая штука!
